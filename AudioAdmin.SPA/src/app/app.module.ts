53a4e20241c168755a1def9f743a8cb4dae755fc
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-20 15:11:34 -0700

    Add components and service

--- AudioAdmin.SPA/src/app/app.module.ts
+++ AudioAdmin.SPA/src/app/app.module.ts
@@ -4,17 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { StarsComponent } from './stars/stars.component';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { HomeComponent } from './home/home.component';
+import { ProductService } from './shared/product.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarsComponent,
+    SearchComponent,
+    ProductDetailComponent,
+    ProductItemComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarrouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
