275930725abb38c9f23919d64f6151a3ddd1011b
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-23 13:24:16 -0700

    Add Initial Product.services.ts code

--- AudioAdmin.SPA/src/app/shared/product.service.ts
+++ AudioAdmin.SPA/src/app/shared/product.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public description: string,
+    public categories: string[]) {
+  }
+}
+
 export class ProductService {
 
-  constructor() { }
+  getProducts(): Product[] {
+    return products.map(p => new Product(p.id, p.title,
+      p.price, p.rating, p.description, p.categories));
+  }
 
+  getProductById(productId: number): Product {
+    return products.find(p => p.id === productId);
+  }
 }
+
+const products = [
+  {
+    'id': 0,
+    'title': 'First Product',
+    'price': 24.99,
+    'rating': 4.3,
+    'description': 'This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['electronics', 'hardware']
+  },
+  {
+    'id': 1,
+    'title': 'Second Product',
+    'price': 64.99,
+    'rating': 3.5,
+    'description': 'This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['books']
+  }];
+
+
