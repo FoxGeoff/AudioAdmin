cef3b72b524a830060e70bf3ab46b61339c02533
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-23 18:13:24 -0700

    Add routing for a product detail

--- AudioAdmin.SPA/src/app/product-detail/product-detail.component.ts
+++ AudioAdmin.SPA/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from '../shared/product.service';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'nga-product-detail',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService) { }
 
   ngOnInit() {
+    const prodId: number = parseInt(
+                   this.route.snapshot.params['productId']);
+    this.product = this.productService.getProductById(prodId);
   }
-
 }
